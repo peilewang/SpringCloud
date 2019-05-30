10d8b97ee9c943ff3837b1ac753371b845e11b57
plw <894872073@qq.com>
2019-05-30 22:55:11 +0800

    SpringCloud

--- microservice-eureka-feign/src/main/java/com/itheima/springcloud/feign/service/UserFeignClient.java
+++ microservice-eureka-feign/src/main/java/com/itheima/springcloud/feign/service/UserFeignClient.java
@@ -4,14 +4,12 @@ import com.itheima.springcloud.feign.entity.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.itheima.springcloud.feign.component.*;
 /**
  * 使⽤@FeignClient("microservice-provider-user")注解绑定micros
  ervice-provider-user服务，还可以使⽤url参数指定⼀个URL。
  * @author eacdy
  */
-//@FeignClient(name = "microserice-eureka-user")
-@FeignClient(name = "microservice-provider-user", fallback = HystrixClientFallback.class)//feign熔断器
+@FeignClient(name = "microserice-eureka-user")
 public interface UserFeignClient {
     @RequestMapping("/{id}")
     public User findByIdFeign(@RequestParam("id") Long id);
