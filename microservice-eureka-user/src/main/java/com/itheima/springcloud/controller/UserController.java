04783e798e0cd72cc8f9a18598ef01662c05c28b
plw <894872073@qq.com>
2020-03-05 09:27:06 +0800

    wwww

--- microservice-eureka-user/src/main/java/com/itheima/springcloud/controller/UserController.java
+++ microservice-eureka-user/src/main/java/com/itheima/springcloud/controller/UserController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class UserController {
     @Autowired
@@ -18,7 +23,7 @@ public class UserController {
      * @RequestMapping(value = "/id", method = RequestMethod.GE
     T)
      * 类似的注解还有@PostMapping等等
-     * @param id
+     * @param
      * @return user信息
      */
     //Swagger2 解决 java.lang.NumberFormatException异常
@@ -34,5 +39,25 @@ public class UserController {
     public ServiceInstance showInfo() {
         ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
         return localServiceInstance;
+    }
+    @GetMapping("/getdd")
+    public Map<String,Object> getexample(){
+        Map<String,Object> map=new HashMap<>();
+        List list=new ArrayList<>();
+        User user=new User();
+        user.setUsername("222");
+        user.setId(2L);
+        user.setAge(5);
+        list.add(user);
+        User user1=new User();
+        user1.setUsername("111");
+        user1.setId(2L);
+        user1.setAge(5);
+        list.add(user1);
+        map.put("code","200");
+        map.put("message","操作成功");
+        map.put("data",list);
+        map.put("user",user);
+        return map;
     }
 }
