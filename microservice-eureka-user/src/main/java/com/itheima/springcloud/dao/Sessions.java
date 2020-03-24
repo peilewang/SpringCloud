cf16e54c758ad10db64632e7baf3bf60289a3d3b
plw <894872073@qq.com>
2020-03-24 18:35:57 +0800

    mybatis自动生成代码

---
+++ microservice-eureka-user/src/main/java/com/itheima/springcloud/dao/Sessions.java
@@ -0,0 +1,55 @@
+package com.itheima.springcloud.dao;
+
+import java.util.Date;
+
+public class Sessions {
+    private Integer id;
+
+    private String app;
+
+    private String usr;
+
+    private Date starttime;
+
+    private Date endtime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getApp() {
+        return app;
+    }
+
+    public void setApp(String app) {
+        this.app = app == null ? null : app.trim();
+    }
+
+    public String getUsr() {
+        return usr;
+    }
+
+    public void setUsr(String usr) {
+        this.usr = usr == null ? null : usr.trim();
+    }
+
+    public Date getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(Date starttime) {
+        this.starttime = starttime;
+    }
+
+    public Date getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(Date endtime) {
+        this.endtime = endtime;
+    }
+}
